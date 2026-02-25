7c0f74415f99fe2a053c9620702873fee34a5e3d
AshenKavinda <kavindahemarathna321@gmail.com>
2026-02-25 23:47:13 +0530

    Fix mixed content using vercel proxy 5

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_API_BASE_URL ?? '';
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "",
   headers: {
     "Content-Type": "application/json",
   },
